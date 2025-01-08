894afc02a4f20b883fb3a500b7eeb30ba262e7df
jschiffres <jamieschiffres@gmail.com>
2025-01-07 21:10:17 -0700

    added new views for add and edit expenses

--- MyWebsite/budget/urls.py
+++ MyWebsite/budget/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("AddExpense", views.add_expense, name="add_expense"),
+    path("EditExpense/<int:expense_id>", views.edit_expense, name="edit_expense"),
 ]
--- MyWebsite/budget/views.py
+++ MyWebsite/budget/views.py
@@ -6,3 +6,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the budget index.")
+
+def add_expense(request):
+    return HttpResponse("New Expense Page")
+
+def edit_expense(request, expense_id):
+    response = "You're looking at the results of expense %s."
+    return HttpResponse(response % expense_id)
